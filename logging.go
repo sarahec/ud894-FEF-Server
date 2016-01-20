8c8ca38e0788ce91af6e9e49b1f29abd36069f49
Sarah Clark <sarahclark@google.com>
2016-01-20 11:38:47 -0800

    Added debug option

--- logging.go
+++ logging.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
 )
+
+const LogMethodAndURL = 1
+const LogWholeRequest = 2
 
 type LoggingResponseWriter struct {
 	http.ResponseWriter
@@ -14,10 +19,24 @@ func (w *LoggingResponseWriter) Write(data []byte) (int, error) {
 	return w.ResponseWriter.Write(data)
 }
 
-func logWrapper(handler http.Handler) http.Handler {
+func logWrapper(level int, handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		writer := &LoggingResponseWriter{w}
-		log.Printf("< %s %s\n", r.Method, r.RequestURI) // TODO use httputil.DumpRequest?
+		switch level {
+		case LogMethodAndURL:
+			log.Printf("< %s %s\n", r.Method, r.RequestURI)
+		case LogWholeRequest:
+			dump, err := httputil.DumpRequest(r, true)
+			if err == nil {
+				fmt.Printf("< %s\n", dump)
+			} else {
+				log.Fatalf("** %s\n", err)
+			}
+		}
+
+		// Keep it simple for now. Could use httputil.DumpRequest for
+		// extra-verbose logging
+
 		handler.ServeHTTP(writer, r)
 	})
 }
--- main.go
+++ main.go
@@ -9,45 +9,55 @@ import (
 
 const filepath = "_data/menu.json"
 const resetpath = "_data/starter_menu.json"
+const noLogging = 0
 
-func addLogging(logging bool, handler http.Handler) http.Handler {
-	if !logging {
+func addLogging(logLevel int, handler http.Handler) http.Handler {
+	if logLevel == noLogging {
 		return handler
 	}
-	return logWrapper(handler)
+	return logWrapper(logLevel, handler)
 }
 
 func main() {
 	port := flag.Int("port", 8000, "server port (on localhost, default 8000")
 	wwwPath := flag.String("www", "_www", "path for serving web files")
 	restPath := flag.String("api", "/api/items", "prefix for REST path")
-	doReset := flag.Bool("reset", false, "reset model from starter file before using")
-	doLog := flag.Bool("log", false, "log REST requests and responses")
-	doSummarize := flag.Bool("verbose", false, "show summary of settings")
+	reset := flag.Bool("reset", false, "reset model from starter file before using")
+	logging := flag.Bool("log", false, "log REST requests (method and URL) and JSON responses")
+	debugRequests := flag.Bool("debug", false, "show details of REST requests and responses (more verbose than -log)")
+	summarize := flag.Bool("verbose", false, "show summary of settings")
 	flag.Parse()
 
-	if *doSummarize {
-		if *doReset {
+	if *summarize {
+		if *reset {
 			fmt.Printf("  Re-loading model from %v\n", resetpath)
 		}
 		fmt.Printf("  Serving on http://localhost:%v/\n", *port)
 		fmt.Printf("  Serving files from %v on /\n", *wwwPath)
 		fmt.Printf("  Serving REST requests on %v\n", *restPath)
-		if *doLog {
+		if *logging {
 			fmt.Printf("  Logging requests and responses for %v\n", *restPath)
 		}
 	}
 
 	menu := &Menu{}
-	if *doReset {
+	if *reset {
 		menu.Load(resetpath)
 	} else {
 		menu.Load(filepath)
 	}
+
+	logLevel := noLogging
+	switch {
+	case *logging:
+		logLevel = LogMethodAndURL
+	case *debugRequests:
+		logLevel = LogWholeRequest
+	}
+
 	http.Handle("/", http.FileServer(http.Dir(*wwwPath)))
 	server := &Server{menu, filepath}
-	handler := addLogging(*doLog, http.StripPrefix(*restPath, server))
+	handler := addLogging(logLevel, http.StripPrefix(*restPath, server))
 	http.Handle(*restPath, handler)
-	// TODO catch and report errors
 	http.ListenAndServe(":"+strconv.Itoa(*port), nil)
 }
